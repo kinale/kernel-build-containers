f9613834715a5678bb53aaca9db3cd8048fac542
Alexander Popov <alex.popov@linux.com>
2026-03-07 20:42:29 +0300

    Refactor preparing to the tests
    Check SRC_DIR and OUT_DIR first of all.
    
    Calculate sha256sum anyway.
    
    Run the container preparing only once.

--- tests_for_build_linux.sh
+++ tests_for_build_linux.sh
@@ -7,11 +7,11 @@ ARCHS=("x86_64" "i386" "arm64" "arm" "riscv" "powerpc" "powerpc64" "powerpc64le"
 COMPILERS=("gcc-13" "clang-15")
 RUNTIME_FLAG=""
 
-SRC_DIR=$PWD/linux-sources
-OUT_DIR=$PWD/build_out
-SRC_TARBALL=$PWD/linux-sources.tar.xz
+SRC_DIR="$PWD/linux_src"
+OUT_DIR="$PWD/build_out"
+SRC_TARBALL="$PWD/linux_src.tar.xz"
 KERNEL="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.65.tar.xz"
-EXPECTED_HASH="54e852667af35c0ed06cfc81311e65fa7f5f798a3bfcf78a559d3b4785a139c1"
+EXPECTED_CHECKSUM="54e852667af35c0ed06cfc81311e65fa7f5f798a3bfcf78a559d3b4785a139c1"
 
 declare -A EXPECTED_IMAGES=(
 	[x86_64]="arch/x86/boot/bzImage"
@@ -29,9 +29,9 @@ fail() {
 	exit 1
 }
 
-prepare_deps() {
+prepare_tests() {
 	echo -e $DELIMITER
-	echo "Preparing dependencies..."
+	echo "Preparing to the tests..."
 
 	for cmd in wget tar expect; do
 		if [ -z "$(command -v "$cmd")" ]; then
@@ -39,37 +39,32 @@ prepare_deps() {
 		fi
 	done
 
-	if [ -f "$SRC_TARBALL" ]; then
-		CALCULATED_HASH=$(sha256sum "$SRC_TARBALL" | awk '{print $1}')
-		if [ "$CALCULATED_HASH" = "$EXPECTED_HASH" ]; then
-			echo "Using existing $SRC_TARBALL (hash verified)"
-		else
-			fail "$SRC_TARBALL exists but hash does not match. Remove $SRC_TARBALL manually to re-download."
-		fi
-	else
+	if [ -d "$SRC_DIR" ] || [ -d "$OUT_DIR" ]; then
+		fail "Some files left from the previous test, remove $SRC_DIR and $OUT_DIR"
+	fi
+
+	if [ ! -f "$SRC_TARBALL" ]; then
 		wget "$KERNEL" -O "$SRC_TARBALL"
 	fi
 
-	if [ -e "$SRC_DIR" ] || [ -e "$OUT_DIR" ]; then
-		fail "Working directory is not clean! Remove $SRC_DIR $OUT_DIR"
+	CHECKSUM=$(sha256sum "$SRC_TARBALL" | awk '{print $1}')
+	if [ "$CHECKSUM" != "$EXPECTED_CHECKSUM" ]; then
+		fail "Unexpected sha256sum of the $SRC_TARBALL, remove $SRC_TARBALL and restart the test"
 	fi
 
 	mkdir -p "$SRC_DIR" "$OUT_DIR"
 	tar -xf "$SRC_TARBALL" -C "$SRC_DIR" --strip-components=1
-}
 
-prepare_compilers() {
-	echo -e $DELIMITER
-	echo "Preparing compilers..."
-	for compiler in "${COMPILERS[@]}"; do
-		python3 manage_images.py $RUNTIME_FLAG -b "$compiler"
+	for COMPILER in "${COMPILERS[@]}"; do
+		python3 manage_images.py -d -b "$COMPILER"
+		python3 manage_images.py -p -b "$COMPILER"
 	done
+
+	echo "[+] Everything is ready for the tests"
 }
 
 run_tests() {
 	# Use $RUNTIME_FLAG without quotes since it may be empty (default value)
-
-	prepare_compilers
 
 	echo -e $DELIMITER
 	echo "Testing kernel building..."
@@ -168,7 +163,7 @@ cleanup() {
 }
 
 echo "Let's test build_linux.py..."
-prepare_deps
+prepare_tests
 python3 -m coverage erase
 
 # Test Docker as a default container runtime (without a flag)
